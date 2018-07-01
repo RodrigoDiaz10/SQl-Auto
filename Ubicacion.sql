8ed1b4eda1646e28bcd4e66566c669e074967dfd
RodrigoDiaz10 <36495069+RodrigoDiaz10@users.noreply.github.com>
2018-07-01 15:52:49 -0500

    Update Ubicacion.sql

--- Ubicacion.sql
+++ Ubicacion.sql
@@ -11,45 +11,45 @@ create table Paises(
 id primary key int not null auto_increment,
 nombre varchar (20),
 id_continente int,
-foreing key (id_continente)references Continentes(id) 
+foreign key (id_continente)references Continentes(id) 
 );
 
 create table Regiones(
 id primary key int not null auto_increment,
 nombre varchar (20),
 id_pais int,
-foreing key (id_pais)references Paises(id) 
+foreign key (id_pais)references Paises(id) 
 );
 
 create table Provincias(
 id primary key int not null auto_increment,
 nombre varchar (20),
 id_region int,
-foreing key (id_region)references Regiones(id) 
+foreign key (id_region)references Regiones(id) 
 );
 
 create table Cantones(
 id primary key int not null auto_increment,
 nombre varchar (20),
 id_provincia int,
-foreing key (id_provincia)references Provincias(id) 
+foreign key (id_provincia)references Provincias(id) 
 );
 
 create table Parroquias(
 id primary key int not null auto_increment,
 nombre varchar (20),
 id_canton int,
-foreing key (id_canton)references Cantones(id) 
+foreign key (id_canton)references Cantones(id) 
 );
 
 create table Barrios(
 id primary key int not null auto_increment,
 nombre varchar (20),
 id_parroquia int,
-foreing key (id_parroquia)references Parroquias(id) 
+foreign key (id_parroquia)references Parroquias(id) 
 );
 
-/* +******+*****+ */
+/* +******+*****+ 
 
 create table Ubicacion_Geografica(
     id primary key int not null auto_increment,
@@ -65,4 +65,4 @@ create table Tipo(
     nombre varchar(20)
 );
 
-/* +******+*****+ */
++******+*****+ */  
